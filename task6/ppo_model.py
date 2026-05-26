7d81cfb04caa90dea2b8d03a2c5cec21196f77ee
dylankprice <dylankenjiprice@gmail.com>
2026-05-26 12:56:37 -0700

    epsilon clipping fix

--- task6/ppo_model.py
+++ task6/ppo_model.py
@@ -24,6 +24,7 @@ class ActorCritic(nn.Module):
         self.critic = nn.Linear(256, 1)
 
     def forward(self, x):
+        h = self.head(x)
         return self.actor(h), self.critic(h)
 
 class Environments:
@@ -147,7 +148,7 @@ def PPO(envs, actorcritic, T=128, K=3, batch_size=256, gamma=0.99,
         loader = DataLoader(TensorDataset(flat_adv, flat_obs, flat_acts, flat_lps, flat_vals),
                             batch_size=batch_size, shuffle=True)
 
-        clip_eps = 0.1 * (1. - iteration / nb_iterations)  # anneal ε
+        clip_eps = 0.1 * (1. - iteration / nb_iterations) 
 
         for _ in range(K):
             for b_adv, b_obs, b_act, b_old_lp, b_old_val in loader:
