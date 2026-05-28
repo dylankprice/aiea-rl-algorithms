8f43f285baed49d07fe66a79ef54ea61a9fa9078
dylankprice <dylankenjiprice@gmail.com>
2026-05-27 18:14:35 -0700

    cnn lin size fix

--- task7/model.py
+++ task7/model.py
@@ -12,7 +12,7 @@ class DQNetwork(nn.Module):
         self.conv = nn.Sequential(
             nn.Conv2d(4, 32, 8, stride=4), nn.ReLU(),
             nn.Conv2d(32, 64, 4, stride=2), nn.ReLU(),
-            nn.Flatten(), nn.Linear(2592, 256), nn.ReLU(),
+            nn.Flatten(), nn.Linear(5184, 256), nn.ReLU(),
         )
         self.fc = nn.Linear(256, nb_actions)
 
